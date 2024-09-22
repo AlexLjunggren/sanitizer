98228c460e344d8980358643267f3cef4bf514e7
Alex Ljunggren <aljunggren@dminc.com>
2024-09-22 14:24:13 -0400

    Formatting

--- src/main/java/io/ljunggren/sanitizer/cleanser/TrimCleanser.java
+++ src/main/java/io/ljunggren/sanitizer/cleanser/TrimCleanser.java
@@ -3,7 +3,7 @@ package io.ljunggren.sanitizer.cleanser;
 public class TrimCleanser implements Cleanser<String> {
 
     public String cleanse(String value) {
-        return value == null? value : value.trim();
+        return value == null ? value : value.trim();
     }
 
     public String description() {
