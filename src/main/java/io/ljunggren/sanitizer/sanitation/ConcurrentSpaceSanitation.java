a9b849b67f447e7bd589cac3ab2bf290dc1518da
Alex Ljunggren <aljunggren@dminc.com>
2024-09-22 14:24:32 -0400

    Refactor cleanser to private variable

--- src/main/java/io/ljunggren/sanitizer/sanitation/ConcurrentSpaceSanitation.java
+++ src/main/java/io/ljunggren/sanitizer/sanitation/ConcurrentSpaceSanitation.java
@@ -10,11 +10,11 @@ import io.ljunggren.sanitizer.cleanser.ReplaceCleanser;
 public class ConcurrentSpaceSanitation extends SanitationChain {
     
     private final String SPACE = " ";
+    private Cleanser<String> cleanser = new ReplaceCleanser(SPACE + SPACE, SPACE);
     
     @Override
     public void sanitize(Annotation annotation, Item item) {
         if (annotation.annotationType() == ConcurrentSpace.class && isString(item)) {
-            Cleanser<String> cleanser = new ReplaceCleanser(SPACE + SPACE, SPACE);
             item.setValue(cleanser.cleanse(item.getValue().toString()));
             return;
         }
